7240c45d69c62558170c9f4ffbd38ae6fa131ac3
Lovish Goel <106914124+learning09@users.noreply.github.com>
2023-03-28 08:24:22 +0530

    Update perturbed_images_generation_multiProcess.py

--- perturbed_images_generation_multiProcess.py
+++ perturbed_images_generation_multiProcess.py
@@ -797,7 +797,7 @@ class perturbed(object):
 				with open(self.save_path+'grey/'+perfix_+'_'+fold_curve+'.gw', 'wb') as f:
 					pickle_perturbed_data = pickle.dumps(synthesis_perturbed_grey)
 					f.write(pickle_perturbed_data)
-				print(synthesis_perturbed_color[:,:,1])	
+				cv2.imwrite(self.save_path+'outputs/'+perfix_+'_'+fold_curve+'.png', grey)	
 				cv2.imwrite(self.save_path+'outputs/'+perfix_+'_'+fold_curve+'.png', synthesis_perturbed_color[:, :, :3])
 
 
